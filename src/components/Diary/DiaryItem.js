0fd8d5cad8ecadee618aff739e6f2ab4314a60aa
Abhishek Verma <av4programming@gmail.com>
2020-12-20 14:13:37 +0530

    added onLoad image upload handler

--- src/components/Diary/DiaryItem.js
+++ src/components/Diary/DiaryItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { fetchDiary } from "../../actions";
 import Badge from "react-bootstrap/Badge";
@@ -6,12 +6,14 @@ import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import DiaryDelete from "./DiaryDelete";
+import Spinner from "react-bootstrap/Spinner";
 import DiaryEdit from "./DiaryEdit";
 import Figure from "react-bootstrap/Figure";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const DiaryItem = (props) => {
+  const [loaded, setLoaded] = useState(false);
   return (
     <Card onClick={() => props.fetchDiary(props.id)}>
       <Accordion.Toggle as={Card.Header} eventKey={props.id}>
@@ -25,17 +27,34 @@ const DiaryItem = (props) => {
         <Card.Body>
           <Row>
             {props.image ? (
-              <Col xs={12} sm={6} md={6} lg={6}>
-                <a href={props.image} target="_blank">
+              <Col xs={12} sm={4} md={6} lg={4}>
+                {loaded ? null : (
+                  <>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                    <small>Loading Image</small>
+                  </>
+                )}
+                <a
+                  href={props.image}
+                  style={loaded ? {} : { display: "none" }}
+                  target="_blank"
+                >
                   <Figure.Image
                     className="image"
                     src={props.image}
+                    onLoad={() => setLoaded(true)}
                     thumbnail
                   ></Figure.Image>
                 </a>
               </Col>
             ) : null}
-            <Col md={6}>
+            <Col xs={12} sm={12} md={12} lg={8}>
               <div className="text my-2">{props.text}</div>
             </Col>
           </Row>
