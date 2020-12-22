8f74c7a280f8ad4ac1b069b4b8f8b30ea31d0fbb
Abhishek Verma <av4programming@gmail.com>
2020-12-22 18:36:50 +0530

    done

--- src/components/auth/FacebookLoginButton.js
+++ src/components/auth/FacebookLoginButton.js
@@ -8,43 +8,11 @@ import Spinner from "react-bootstrap/Spinner";
 
 const FacebookLoginButton = (props) => {
   return (
-    // <FacebookLogin
-    //   appId="1088597931155576"
-    //   fields="name,email,picture"
-    //   callback={props.facebookLogin}
-    //   render={(renderProps) => (
-    //     <button onClick={renderProps.onClick}>
-    //       This is my custom FB button
-    //     </button>
-
-    //           <button
-    //     className="google-button btn-block"
-    //     onClick={renderProps.onClick}
-    //     disabled={renderProps.disabled}
-    //   >
-    //     <span className="google-button__icon">
-    //       {props.btnLoading ? (
-    //         <Spinner
-    //           style={{ display: "block" }}
-    //           as="span"
-    //           animation="border"
-    //           size="sm"
-    //           role="status"
-    //           aria-hidden="true"
-    //         />
-    //       ) : (
-    //         <img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/F_icon.svg" />
-    //       )}
-    //     </span>
-    //     <span className="google-button__text">Sign in with Facebook</span>
-    //   </button>
-    // )}
-    //   )}
-    // />
     <FacebookLogin
       appId="3375293559265744"
       fields="name,email,picture"
       disableMobileRedirect={true}
+      isMobile={false}
       render={(renderProps) => (
         <button
           className="google-button btn-block"
