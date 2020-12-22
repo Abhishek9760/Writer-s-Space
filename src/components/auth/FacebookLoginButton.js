329491a1e698cb239d211a51e0141d6359bba694
Abhishek Verma <av4programming@gmail.com>
2020-12-22 20:03:05 +0530

    done

--- src/components/auth/FacebookLoginButton.js
+++ src/components/auth/FacebookLoginButton.js
@@ -8,50 +8,36 @@ import Spinner from "react-bootstrap/Spinner";
 
 const FacebookLoginButton = (props) => {
   return (
-    // <FacebookLogin
-    //   appId="3375293559265744"
-    //   fields="name,email,picture"
-    //   // disableMobileRedirect={true}
-    //   redirectUri="https://laughing-bose-a55b10.netlify.app/diary/"
-    //   cookie
-    //   isMobile={false}
-    //   render={(renderProps) => (
-    //     <button
-    //       className="google-button btn-block"
-    //       onClick={renderProps.onClick}
-    //       disabled={renderProps.disabled}
-    //     >
-    //       <span className="google-button__icon">
-    //         {props.btnLoading ? (
-    //           <Spinner
-    //             style={{ display: "block" }}
-    //             as="span"
-    //             animation="border"
-    //             size="sm"
-    //             role="status"
-    //             aria-hidden="true"
-    //           />
-    //         ) : (
-    //           <img src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Facebook_circle_pictogram.svg" />
-    //         )}
-    //       </span>
-    //       <span className="google-button__text">Sign in with Facebook</span>
-    //     </button>
-    //   )}
-    //   callback={(res) => props.facebookLogin(res)}
-    // />
     <FacebookLogin
-      socialId="3375293559265744"
-      language="en_US"
-      scope="public_profile,email"
-      responseHandler={(res) => props.facebookLogin(res)}
-      xfbml={true}
-      fields="id,email,name"
-      version="v2.5"
-      className="facebook-login"
-      buttonText="Login With Facebook"
+      appId="3375293559265744"
+      fields="name,email,picture"
+      disableMobileRedirect={true}
+      redirectUri="https://laughing-bose-a55b10.netlify.app/diary/"
+      render={(renderProps) => (
+        <button
+          className="google-button btn-block"
+          onClick={renderProps.onClick}
+          disabled={renderProps.disabled}
+        >
+          <span className="google-button__icon">
+            {props.btnLoading ? (
+              <Spinner
+                style={{ display: "block" }}
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+            ) : (
+              <img src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Facebook_circle_pictogram.svg" />
+            )}
+          </span>
+          <span className="google-button__text">Sign in with Facebook</span>
+        </button>
+      )}
+      callback={(res) => props.facebookLogin(res)}
     />
-    // </div>
   );
 };
 
