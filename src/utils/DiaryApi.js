7a81326df22fbde618c77f67b10ea6961b3acfbb
Abhishek Verma <av4programming@gmail.com>
2020-12-23 19:09:24 +0530

    added loading

--- src/utils/DiaryApi.js
+++ src/utils/DiaryApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://abhi102.pythonanywhere.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
