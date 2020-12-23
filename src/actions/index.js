42c856d20b7d1475d9c0a6dd477f82b042874506
Abhishek Verma <av4programming@gmail.com>
2020-12-23 19:37:00 +0530

    done

--- src/actions/index.js
+++ src/actions/index.js
@@ -275,7 +275,7 @@ export const createDiary = (formValues) => async (dispatch, getState) => {
     })
     .then(() => {
       if (toastId) {
-        toast.done(toast);
+        toast.done(toastId);
       }
       if (formValues.image) {
         success("Image upload complete.");
@@ -335,7 +335,7 @@ export const editDiary = (id, formValues) => async (dispatch, getState) => {
     },
   });
   if (toastId) {
-    toast.done(toast);
+    toast.done(toastId);
   }
   success("Saved");
   dispatch({ type: LOADING, Loading: false });
