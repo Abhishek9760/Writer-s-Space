cb11a8cdc9fb31e5d90e7f57064cd8f311afd6d5
Abhishek Verma <av4programming@gmail.com>
2021-01-12 18:08:33 +0530

    done

--- src/components/404.js
+++ src/components/404.js
@@ -13,10 +13,28 @@ class NotFound extends React.Component {
         <Col sm={12} xs={12} xl={6} md={12} lg={6}>
           <Image src="error-404.svg" fluid></Image>
         </Col>
-        <Col xs={12} sm={12} xl={6} md={12} lg={6}>
+        <Col
+          xs={12}
+          sm={12}
+          xl={6}
+          md={12}
+          lg={6}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "64px",
+          }}
+        >
           <p
             class="display-4"
-            style={{ fontWeight: "100", textAlign: "center", color: "white" }}
+            style={{
+              // fontWeight: "100",
+              color: "white",
+              fontSize: "3rem",
+              "-webkit-text-fill-color": "transparent",
+              "-webkit-text-stroke": "2px black",
+            }}
           >
             Not Found
           </p>
